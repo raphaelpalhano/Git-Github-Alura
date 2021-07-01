ff6b5f9f441d935f960d044a4252a1ba0b972e4a
raphaelpalhano <raphael1angel@hotmail.com>
2021-06-30 23:14:41 -0300

    pgMain

--- src/test/java/Page/PageMain.java
+++ src/test/java/Page/PageMain.java
@@ -1,18 +1,13 @@
 package Page;
 
-<<<<<<< HEAD
-=======
-
->>>>>>> header
 import org.openqa.selenium.WebDriver;
+
 
 public class PageMain {
 	WebDriver driver;
 	String URL;
 	
 
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> header
+
+
